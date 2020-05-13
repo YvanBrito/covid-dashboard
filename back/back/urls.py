0f24168a151605ceee89abb2a004a73d5b9e41c9
Yvan Brito <yvanbrito96@gmail.com>
2020-05-13 09:03:43 -0300

    Configurando acesso do back ao front

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,16 +1,18 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 from rest_framework import routers
 from back.covid import views
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
+# path('', include(router.urls)),
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('global/', views.globalCases, name='cases'),
     path('brasil/', views.brasilCases, name='brasilCases'),
     path('para/', views.globalCases, name='cases'),
     path('hello2/', views.hellodois, name='hello2'),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
